cc4119933c84fc728b740749a8e305337b8b8b26
Victor Huamaní León <archivos.totalquality01@gmail.com>
2026-09-19 11:07:39 -0500

    Update 01. Precisión_repetibilidad_un_nivel.py

--- CURSO PYTHON PARA LABORATORIOS DE ENSAYOS/CURSO 02/01. Precisión_repetibilidad_un_nivel.py
+++ CURSO PYTHON PARA LABORATORIOS DE ENSAYOS/CURSO 02/01. Precisión_repetibilidad_un_nivel.py
@@ -1,1 +1,24 @@
+#Cálculo de la desviación estándar de repetibilidad (sr) para un nivel de trabajo
 
+import pandas as pd
+import numpy as np
+
+# 1. Importar datos
+datos = pd.read_excel(r"D:\Ciencia_de_Datos\Validación de métodos químicos\precision.xlsx",sheet_name="hoja1")
+
+# 2. Desviación estándar de cada analista
+estadisticas = datos.describe()
+
+# 3. Extraer las desviaciones estándar
+s = estadisticas.loc["std"]
+
+# 4. Número de resultados de cada analista
+n = datos.count()
+
+# 5. Calcular desviación estándar de repetibilidad
+Sr = np.sqrt(
+    np.sum((n - 1) * s**2) /
+    np.sum(n - 1)
+)
+
+print(f"Desviación estándar de repetibilidad (Sr) = {Sr:.4f} mg/L")
